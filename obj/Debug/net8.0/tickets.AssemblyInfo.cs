f24f1a4bf084681b1d1b22ff6817fe5299b16185
tshepoandi <lovekilledtshepo@gmail.com>
2024-10-17 10:33:26 +0200

    some debugging

--- obj/Debug/net8.0/tickets.AssemblyInfo.cs
+++ obj/Debug/net8.0/tickets.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tickets")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a074c0826fbf20d9e055d0589ecb11fcc2251ab3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+597d34a6709c70232528baf30ed8ab742b49a57d")]
 [assembly: System.Reflection.AssemblyProductAttribute("tickets")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tickets")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
